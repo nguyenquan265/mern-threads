e09dc89443a8aa19b32c8a6feaf4ed589a209880
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-06-21 16:31:22 +0700

    basic setup

---
+++ client/src/components/UserHeader.jsx
@@ -0,0 +1,11 @@
+import { Box, VStack } from '@chakra-ui/layout'
+
+const UserHeader = () => {
+  return (
+    <VStack gap='4' alignItems='start'>
+      <Flex></Flex>
+    </VStack>
+  )
+}
+
+export default UserHeader
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+import { extendTheme } from '@chakra-ui/theme-utils'
+import { ColorModeScript } from '@chakra-ui/color-mode'
+
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+      bg: mode('gray.100', '#101010')(props)
+    }
+  })
+}
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true
+}
+
+const colors = {
+  gray: {
+    light: '#616161',
+    dark: '#1e1e1e'
+  }
+}
+
+const theme = extendTheme({ styles, config, colors })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
 )
